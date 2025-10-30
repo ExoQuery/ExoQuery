d63731b60721e53da28919c77e25cb0ef52ff9e2
Alexander Ioffe <deusaquilus@gmail.com>
2025-10-30 08:46:33 -0400

    Remove ExoGoldenTest

--- testing/src/commonTest/kotlin/io/exoquery/QueryReq.kt
+++ testing/src/commonTest/kotlin/io/exoquery/QueryReq.kt
@@ -8,7 +8,7 @@ import io.exoquery.testdata.Robot
 // Also note that it won't actually override the BasicQuerySanitySpecGolden file unless you change this one
 
 // build the file BasicQuerySanitySpecGolden.kt, is that as the constructor arg
-class QueryReq: GoldenSpecDynamic(QueryReqGoldenDynamic, Mode.ExoGoldenOverride(), {
+class QueryReq: GoldenSpecDynamic(QueryReqGoldenDynamic, Mode.ExoGoldenTest(), {
   data class Person(val id: Int, val name: String, val age: Int)
   data class Address(val ownerId: Int, val street: String, val city: String)
 
