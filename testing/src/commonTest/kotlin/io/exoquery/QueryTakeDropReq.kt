1f1fe3087593a39bc927a454da2674e72c4131a8
Alexander Ioffe <deusaquilus@gmail.com>
2025-11-23 01:25:15 -0500

    Reducing select{where}.filter and testing another error condition

--- testing/src/commonTest/kotlin/io/exoquery/QueryTakeDropReq.kt
+++ testing/src/commonTest/kotlin/io/exoquery/QueryTakeDropReq.kt
@@ -1,12 +1,18 @@
 package io.exoquery
 
-class QueryTakeDropReq: GoldenSpecDynamic(GoldenQueryFile.Empty, Mode.ExoGoldenOverride(), {
+class QueryTakeDropReq: GoldenSpecDynamic(QueryTakeDropReqGoldenDynamic, Mode.ExoGoldenTest(), {
   data class Person(val name: String, val age: Int)
 
   "take" - {
     "sqlite" {
       val query = sql { Table<Person>().take(4) }
       val result = query.buildFor.Sqlite()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+    "postgres" {
+      val query = sql { Table<Person>().take(4) }
+      val result = query.buildFor.Postgres()
       shouldBeGolden(query.xr, "XR")
       shouldBeGolden(result, "SQL")
     }
@@ -19,12 +25,24 @@ class QueryTakeDropReq: GoldenSpecDynamic(GoldenQueryFile.Empty, Mode.ExoGoldenO
       shouldBeGolden(query.xr, "XR")
       shouldBeGolden(result, "SQL")
     }
+    "postgres" {
+      val query = sql { Table<Person>().drop(4) }
+      val result = query.buildFor.Postgres()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
   }
 
   "take and drop" - {
     "sqlite" {
-      val query = sql { Table<Person>().take(4).drop(4) }
+      val query = sql { Table<Person>().take(3).drop(2) }
       val result = query.buildFor.Sqlite()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+    "postgres" {
+      val query = sql { Table<Person>().take(3).drop(2) }
+      val result = query.buildFor.Postgres()
       shouldBeGolden(query.xr, "XR")
       shouldBeGolden(result, "SQL")
     }
@@ -32,8 +50,14 @@ class QueryTakeDropReq: GoldenSpecDynamic(GoldenQueryFile.Empty, Mode.ExoGoldenO
 
   "drop and limit" - {
     "sqlite" {
-      val query = sql { Table<Person>().drop(4).limit(4) }
+      val query = sql { Table<Person>().drop(2).limit(3) }
       val result = query.buildFor.Sqlite()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+    "postgres" {
+      val query = sql { Table<Person>().drop(2).limit(3) }
+      val result = query.buildFor.Postgres()
       shouldBeGolden(query.xr, "XR")
       shouldBeGolden(result, "SQL")
     }
@@ -41,8 +65,14 @@ class QueryTakeDropReq: GoldenSpecDynamic(GoldenQueryFile.Empty, Mode.ExoGoldenO
 
   "offset and limit" - {
     "sqlite" {
-      val query = sql { Table<Person>().offset(4).limit(4) }
+      val query = sql { Table<Person>().offset(2).limit(3) }
       val result = query.buildFor.Sqlite()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+    "postgres" {
+      val query = sql { Table<Person>().offset(2).limit(3) }
+      val result = query.buildFor.Postgres()
       shouldBeGolden(query.xr, "XR")
       shouldBeGolden(result, "SQL")
     }
@@ -50,8 +80,14 @@ class QueryTakeDropReq: GoldenSpecDynamic(GoldenQueryFile.Empty, Mode.ExoGoldenO
 
   "limit and drop" - {
     "sqlite" {
-      val query = sql { Table<Person>().limit(4).drop(4) }
+      val query = sql { Table<Person>().limit(3).drop(2) }
       val result = query.buildFor.Sqlite()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+    "postgres" {
+      val query = sql { Table<Person>().limit(3).drop(2) }
+      val result = query.buildFor.Postgres()
       shouldBeGolden(query.xr, "XR")
       shouldBeGolden(result, "SQL")
     }
@@ -59,8 +95,14 @@ class QueryTakeDropReq: GoldenSpecDynamic(GoldenQueryFile.Empty, Mode.ExoGoldenO
 
   "limit and offset" - {
     "sqlite" {
-      val query = sql { Table<Person>().limit(4).offset(4) }
+      val query = sql { Table<Person>().limit(3).offset(2) }
       val result = query.buildFor.Sqlite()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+    "postgres" {
+      val query = sql { Table<Person>().limit(3).offset(2) }
+      val result = query.buildFor.Postgres()
       shouldBeGolden(query.xr, "XR")
       shouldBeGolden(result, "SQL")
     }
@@ -68,8 +110,83 @@ class QueryTakeDropReq: GoldenSpecDynamic(GoldenQueryFile.Empty, Mode.ExoGoldenO
 
   "take and offset" - {
     "sqlite" {
-      val query = sql { Table<Person>().take(4).offset(4) }
+      val query = sql { Table<Person>().take(3).offset(2) }
       val result = query.buildFor.Sqlite()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+    "postgres" {
+      val query = sql { Table<Person>().take(3).offset(2) }
+      val result = query.buildFor.Postgres()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+  }
+
+  // Additional chaining case
+  "drop, take, then drop" - {
+    "sqlite" {
+      val query = sql { Table<Person>().drop(2).take(3).drop(1) }
+      val result = query.buildFor.Sqlite()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+    "postgres" {
+      val query = sql { Table<Person>().drop(2).take(3).drop(1) }
+      val result = query.buildFor.Postgres()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+  }
+
+  // This should be (Select * from SqlQuery.limit(5)).offset(2).limit(2) or .limit(2).offset(2)
+  // The inner limit needs to be set first because unlike, limit take is eager (i.e. take it now as opposed to
+  // taking it from the total). After we do the inner query it doesn't mater whether limit or offset is applied first
+  // because the `take` operator is applied last.
+  "take, drop, then take" - {
+    "sqlite" {
+      val query = sql { Table<Person>().take(5).drop(2).take(2) }
+      val result = query.buildFor.Sqlite()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+    "postgres" {
+      val query = sql { Table<Person>().take(5).drop(2).take(2) }
+      val result = query.buildFor.Postgres()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+  }
+
+  // In this case, the limit and offset should be applied first (and it doesn't matter which order)
+  // since `limit` is lazy i.e. it applies to the total result set. Then it needs to be nested
+  // and the outer limit taken from that.
+  "limit, offset, then limit" - {
+    "sqlite" {
+      val query = sql { Table<Person>().limit(5).offset(2).limit(2) }
+      val result = query.buildFor.Sqlite()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+    "postgres" {
+      val query = sql { Table<Person>().limit(5).offset(2).limit(2) }
+      val result = query.buildFor.Postgres()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+  }
+
+  // The first two takes should be nested, then the drop applied to the result
+  "take, take, drop" - {
+    "sqlite" {
+      val query = sql { Table<Person>().take(5).take(3).drop(2) }
+      val result = query.buildFor.Sqlite()
+      shouldBeGolden(query.xr, "XR")
+      shouldBeGolden(result, "SQL")
+    }
+    "postgres" {
+      val query = sql { Table<Person>().take(5).take(3).drop(2) }
+      val result = query.buildFor.Postgres()
       shouldBeGolden(query.xr, "XR")
       shouldBeGolden(result, "SQL")
     }
