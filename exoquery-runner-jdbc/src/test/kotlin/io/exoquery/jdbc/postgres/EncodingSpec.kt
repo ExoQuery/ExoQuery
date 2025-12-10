f7a2d98fda4f4d8f0fadbf1d3ac4f09b65947b95
Alexander Ioffe <deusaquilus@gmail.com>
2025-12-09 19:27:24 -0500

    Fix param(byte) and param(byteArray)

--- exoquery-runner-jdbc/src/test/kotlin/io/exoquery/jdbc/postgres/EncodingSpec.kt
+++ exoquery-runner-jdbc/src/test/kotlin/io/exoquery/jdbc/postgres/EncodingSpec.kt
@@ -6,6 +6,7 @@ import io.exoquery.controller.TerpalSqlUnsafe
 import io.exoquery.controller.runActionsUnsafe
 import io.exoquery.jdbc.TestDatabases
 import io.exoquery.jdbc.encodingdata.JavaTestEntity
+import io.exoquery.jdbc.encodingdata.EncodingTestEntity
 import io.exoquery.jdbc.encodingdata.verify
 import io.kotest.core.spec.style.FreeSpec
 
@@ -16,7 +17,8 @@ class EncodingSpec: FreeSpec({
   beforeEach {
     ctx.runActionsUnsafe(
       """
-      DELETE FROM JavaTestEntity
+      DELETE FROM JavaTestEntity;
+      DELETE FROM EncodingTestEntity
       """
     )
   }
@@ -44,6 +46,76 @@ class EncodingSpec: FreeSpec({
         insert<JavaTestEntity> { setParams(ent) }
       }.buildFor.Postgres().runOn(ctx)
       val res = sql { Table<JavaTestEntity>() }.buildFor.Postgres().runOn(ctx).let { it.firstOrNull() ?: error("Expected one element list but got: ${it}") }
+      verify(res, ent)
+    }
+  }
+  "Encode/Decode EncodingTestEntity" - {
+    val ent = EncodingTestEntity.regular
+    "insert - ctx" {
+      sql {
+        insert<EncodingTestEntity> {
+          set(
+            stringMan to param(ent.stringMan),
+            booleanMan to param(ent.booleanMan),
+            byteMan to param(ent.byteMan),
+            shortMan to param(ent.shortMan),
+            intMan to param(ent.intMan),
+            longMan to param(ent.longMan),
+            floatMan to param(ent.floatMan),
+            doubleMan to param(ent.doubleMan),
+            byteArrayMan to param(ent.byteArrayMan),
+            customMan to param(ent.customMan),
+            stringOpt to param(ent.stringOpt),
+            booleanOpt to param(ent.booleanOpt),
+            byteOpt to param(ent.byteOpt),
+            shortOpt to param(ent.shortOpt),
+            intOpt to param(ent.intOpt),
+            longOpt to param(ent.longOpt),
+            floatOpt to param(ent.floatOpt),
+            doubleOpt to param(ent.doubleOpt),
+            byteArrayOpt to param(ent.byteArrayOpt),
+            customOpt to param(ent.customOpt)
+          )
+        }
+      }.buildFor.Postgres().runOn(ctx)
+      val res = sql { Table<EncodingTestEntity>() }.buildFor.Postgres().runOn(ctx).let { it.firstOrNull() ?: error("Expected one element list but got: ${it}") }
+      verify(res, ent)
+    }
+    "insert - ctx" {
+      sql {
+        insert<EncodingTestEntity> {
+          set(
+            stringMan to param(ent.stringMan),
+            booleanMan to param(ent.booleanMan),
+            byteMan to paramCtx(ent.byteMan),
+            shortMan to param(ent.shortMan),
+            intMan to param(ent.intMan),
+            longMan to param(ent.longMan),
+            floatMan to param(ent.floatMan),
+            doubleMan to param(ent.doubleMan),
+            byteArrayMan to paramCtx(ent.byteArrayMan),
+            customMan to param(ent.customMan),
+            stringOpt to param(ent.stringOpt),
+            booleanOpt to param(ent.booleanOpt),
+            byteOpt to paramCtx(ent.byteOpt),
+            shortOpt to param(ent.shortOpt),
+            intOpt to param(ent.intOpt),
+            longOpt to param(ent.longOpt),
+            floatOpt to param(ent.floatOpt),
+            doubleOpt to param(ent.doubleOpt),
+            byteArrayOpt to paramCtx(ent.byteArrayOpt),
+            customOpt to param(ent.customOpt)
+          )
+        }
+      }.buildFor.Postgres().runOn(ctx)
+      val res = sql { Table<EncodingTestEntity>() }.buildFor.Postgres().runOn(ctx).let { it.firstOrNull() ?: error("Expected one element list but got: ${it}") }
+      verify(res, ent)
+    }
+    "insert - setParams" {
+      sql {
+        insert<EncodingTestEntity> { setParams(ent) }
+      }.buildFor.Postgres().runOn(ctx)
+      val res = sql { Table<EncodingTestEntity>() }.buildFor.Postgres().runOn(ctx).let { it.firstOrNull() ?: error("Expected one element list but got: ${it}") }
       verify(res, ent)
     }
   }
